6dae5de7ec208e9a8eb656127aab6a0ae169e114
Adolfo Alvarez <adolfo.alvar3z.p@gmail.com>
2023-06-23 17:54:29 -0400

    web

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_060611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "timescaledb"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "house"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
